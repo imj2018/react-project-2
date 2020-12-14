8078344e08152fa080b09c2907a656f322397c26
imj <imj.programming.2013@gmail.com>
2020-12-14 13:45:25 +1100

    fdvdvdf

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component.jsx';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-       <Homepage />
+    <Route exact path='/' component = {Homepage} /> 
+    <Route exact path='/hats' component = {Homepage} />
+      <Homepage />
     </div>
 
   );
